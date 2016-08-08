df08107dea48bd33f6d3d214e5b2cdce0d09d358
Rubén López <ruben@cs1.com>
2016-08-07 22:37:10 -0600

    BF Use :includes to increase db productivity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @vehicles = Vehicle.all
+    @vehicles = Vehicle.includes(:location).all
 
     @vehicles_in_groups = @vehicles.in_groups_of(3)
   end
